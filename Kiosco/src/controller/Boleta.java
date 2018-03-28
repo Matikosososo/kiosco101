72bf029255c6f64e9f8aeb063ff62a4f468dda2f
infectus <infectus@infectus-HP>
2018-03-28 16:30:15 -0600

    Definiendo atributos y metodos de las clases!

--- Kiosco/src/controller/Boleta.java
+++ Kiosco/src/controller/Boleta.java
@@ -1,5 +1,33 @@
 package controller;
 
 public class Boleta {
+    private int id;
+    private String fecha;
+
+    public Boleta(int id, String fecha) {
+        this.id = id;
+        this.fecha = fecha;
+    }
+
+    public Boleta() {
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
     
 }
